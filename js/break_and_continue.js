c06a357598d6e48da2b350fc121442e3cf18379f
Nadia Botello <botello.nadia.g@gmail.com>
2019-04-10 16:53:46 -0500

    end of day push

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -5,7 +5,7 @@ var userInput = prompt("Please enter an odd number between 1 and 50");
 for (var i = 1; i <= 50; i += 2) {
 
         if (userInput % 2 === 0) {
-            prompt("Please enter an odd number between 1 and 50")
+            prompt("Please enter an odd number between 1 and 50");
         } else if (i === Number(userInput)){
             console.log("Yikes! Skipping number: " + userInput);
         } else {
@@ -14,3 +14,43 @@ for (var i = 1; i <= 50; i += 2) {
 }
 
 
+// while(true) {
+//
+//     var userChoice = prompt("Please enter an odd number between 1 and 50");
+//
+//     if (isNaN(userChoice)) {
+//         alert("Invalid Input! The input must be a number.");
+//         continue;
+//     }
+//
+//     userChoice = parseFloat(userChoice);
+//
+//     if (userChoice < 1 || userChoice > 50) {
+//         alert("Invalid input! The number entered is out of range.");
+//         continue;
+//     }
+//
+//     if (userChoice % 2 === 0) {
+//         alert("Invalid input! The number entered must be odd.");
+//     } else {
+//         break;
+//     }
+//
+// }
+
+// for (var i = 1; i <= 50; ++i) {
+//
+//     if (i % 2 === 0) {
+//         continue;
+//     }
+//
+//     if (i === userChoice) {
+//         console.log("Yikes! Skipping number: " + i);
+//     } else {
+//         console.log("Here is an odd number: " + i);
+//     }
+//
+//     if (i > 49) {
+//         break;
+//     }
+// }
--- js/while.js
+++ js/while.js
@@ -1,11 +1,11 @@
 "use strict";
 
-// var i = 2;
-//
-// while (i <= 65536){
-//     console.log(i);
-//     i *= 2;
-// }
+var i = 2;
+
+while (i <= 65536){
+    console.log(i);
+    i *= 2;
+}
 
 // An ice cream seller can't go home until she sells all of her cones.
 // Write a JS program that generates a random number between 50 and 100 representing the amount of cones to sell.
@@ -34,8 +34,20 @@ do {
     }
 
     else if (allCones - randomNum < 0) {
-        console.log("Cannot sell you " + randomNum + " cones because I only have " + allCones);
+        console.log("Cannot sell you " + randomNum + " cones because I have " + allCones);
     }
 
 } while (allCones > 0);
 console.log("All gone");
+
+// var conesToSell = Math.floor(Math.random() * 50) + 50;
+// while (conesToSell > 0) {
+//     var conesOrdered = Math.floor(Math.random() * 5) + 1;
+//     if (conesToSell >= conesOrdered) {
+//         console.log(conesToSell + " cones sold!");
+//         conesToSell -= conesOrdered;
+//     } else {
+//         console.log("Cannot sell you " + conesOrdered + " cones. I only have " + conesToSell + "...");
+//     }
+// }
+// console.log("Yay! I sold them all!");
