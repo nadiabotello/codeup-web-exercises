531a36e39732e85260ad1c8007f10d01f45520d2
Nadia Botello <botello.nadia.g@gmail.com>
2019-04-12 11:50:30 -0500

    in progress math object

--- js/circle.js
+++ js/circle.js
@@ -7,20 +7,23 @@
 
         getArea: function () {
             // TODO: complete this method
-            // hint: area = pi * radius^2
-
-            return; // TODO: return the proper value
+            // TODO: return the proper value
+            return (Math.PI * circle.radius^2);
         },
 
         logInfo: function (doRounding) {
             // TODO: complete this method.
-
             // If doRounding is true, round the result to the nearest integer.
             // Otherwise, output the complete value
 
-            console.log("Area of a circle with radius: " + this.radius + ", is: ");
+            if (doRounding === true) {
+                return Math.round(circle.getArea());
+            } else {
+                return console.log("Area of a circle with radius: " + this.radius + ", is: " + circle.getArea());
+            }
         }
     };
+
 
     // log info about the circle
     console.log("Raw circle information");
@@ -29,11 +32,16 @@
     circle.logInfo(true);
 
     console.log("=======================================================");
+
     // TODO: Change the radius of the circle to 5.
+    circle.radius = 5;
+
 
     // log info about the circle
     console.log("Raw circle information");
     circle.logInfo(false);
     console.log("Circle information rounded to the nearest whole number");
     circle.logInfo(true);
+
+
 })();
