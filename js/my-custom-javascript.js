309e408ce6395370a41343d15b3398bc595f1689
Nadia Botello <botello.nadia.g@gmail.com>
2019-04-23 10:33:29 -0500

    toggly cat

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -8,5 +8,16 @@ $('h1, p, li')
     .css('background', 'yellow')
     .css('text-align', 'center');
 
+$('img')
+    .css('display', 'block')
+    .css('margin-left', 'auto')
+    .css('margin-right', 'auto');
+
+function animateIt() {
+    $( "img" ).slideToggle( "slow", animateIt );
+}
+
+animateIt();
+
 var contents = $('h1').html();
 alert(contents);
