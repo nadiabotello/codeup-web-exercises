e7ea297188d5e3953fef62984c0c11944698ecc1
Nadia Botello <botello.nadia.g@gmail.com>
2019-04-23 10:07:27 -0500

    finished element selectors section

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -1,8 +1,10 @@
 "use strict";
 
-$('.codeup').css('border', '1px solid red');
-
 $('img').css('height', '300px');
 
-// var contents = $('p').html();
-// alert(contents);
+$('li').css('font-size', '20px');
+
+$('h1, p, li').css('background', 'yellow');
+
+var contents = $('h1').html();
+alert(contents);
