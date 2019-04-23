b8208057cd4b9b0451ab388460794851b9cb0d65
Nadia Botello <botello.nadia.g@gmail.com>
2019-04-23 10:08:51 -0500

    finished exercise

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -4,7 +4,9 @@ $('img').css('height', '300px');
 
 $('li').css('font-size', '20px');
 
-$('h1, p, li').css('background', 'yellow');
+$('h1, p, li')
+    .css('background', 'yellow')
+    .css('text-align', 'center');
 
 var contents = $('h1').html();
 alert(contents);
