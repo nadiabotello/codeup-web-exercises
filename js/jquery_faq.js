c89d38fae0c8148dd69c827bc878869f8fd43c58
Nadia Botello <botello.nadia.g@gmail.com>
2019-04-24 16:53:05 -0500

    end of day push traversing exercise

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -2,20 +2,40 @@
 
 $(document).ready(function() {
 
+    // Create a button that, when clicked, makes the last li in each ul have a yellow background.
+
     $('#toggle').click(function(event) {
         event.preventDefault();
-        $('.invisible').toggleClass('visible');
+        $('ul > li:last-child').css('background-color','yellow');
     });
 
-    $('dt').click(function(event) {
-        event.preventDefault();
-        $(this).toggleClass('highlighted');
+    // When any h3 is clicked, the list underneath it should be bolded. Use font-weight: bold to achieve this.
+
+    $('h3').click(function(event) {
+        $(this).next().css('font-weight', 'bold');
+    });
+
+
+    // When any list item is clicked, first li of the parent ul should have a font color of blue.
+
+    $('li').click(function(event) {
+        $('ul > li:first-child').css("color", "blue");
 
     });
 
 
+    // Create 3 divs that each look like a picture Frame; each one should have a unique background
+    // image and a button underneath that swaps the image from the frame. Use Traversing functions
+    // of jQuery to accomplish this.
+    //
+    //     The rules are the following:
+    //
+    //     The left frame swaps to the right and takes the image from the frame in the center.
+    //     The center frame swaps randomly to either the left or right.
+    //     The right frame swaps to the left and takes the image from the frame in the center.
 
-    // When any one dt is clicked, the element that was clicked should be highlighted.
+
+
 
 
 
