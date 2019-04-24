beaf567edcb9b3f94429566e48336a1394dc3ec0
Nadia Botello <botello.nadia.g@gmail.com>
2019-04-24 14:45:29 -0500

    finished exercise with bonus

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -1,1 +1,23 @@
 "use strict";
+
+$(document).ready(function() {
+
+    $('#toggle').click(function(event) {
+        event.preventDefault();
+        $('.invisible').toggleClass('visible');
+    });
+
+    $('dt').click(function(event) {
+        event.preventDefault();
+        $(this).toggleClass('highlighted');
+
+    });
+
+
+
+    // When any one dt is clicked, the element that was clicked should be highlighted.
+
+
+
+
+});
