1a0438b10173267b6033d4162fe83d05a7c97990
Nadia Botello <botello.nadia.g@gmail.com>
2019-05-01 11:46:42 -0500

    3 day, icons, map

--- js/weather_map.js
+++ js/weather_map.js
@@ -14,9 +14,70 @@ var seconds = 1556720525;
 var dateObject = new Date(seconds * 1000);
 console.log(dateObject.toString());
 
+var icons = {
+    "clear-day" : "icon/SVG/Sun.svg",
+    "clear-night" : "icon/SVG/Moon-Full.svg",
+    "rain" : "icon/SVG/Cloud-Rain.svg",
+    "snow" : "icon/SVG/Cloud-Snow.svg",
+    "sleet" : "icon/SVG/Cloud-Hail.svg",
+    "wind" : "icon/SVG/Wind.svg",
+    "fog" : "icon/SVG/Cloud-Fog-Alt.svg",
+    "cloudy" : "icon/SVG/Cloud.svg",
+    "partly-cloudy-day" : "icon/SVG/Cloud-Sun.svg",
+    "partly-cloudy-night" : "icon/SVG/Cloud-Moon.svg"
+    };
 
-$.get(corsUrl + darkSkyUrl + darkSkyAPIKey + "/" + lat + "," + lng + "/" + lang).done(function(data) {
+// $.get(corsUrl + darkSkyUrl + darkSkyAPIKey + "/" + lat + "," + lng + "/" + lang).done(function(data) {
+//     console.log(data);
+// });
+
+function postsHtml(posts) {
+
+    var html = '';
+    var i;
+
+    for (i = 0; i <= 2; i++) {
+        html += '<div class="currentSA">';
+        html += '<h1>' + posts.daily.data[i].temperatureHigh + "/";
+        html += posts.daily.data[i].temperatureLow + '</h1>';
+        html += '<img src="' + icons[posts.daily.data[i].icon] + '">';
+        html += '<h5>Clouds: ' + posts.daily.data[i].cloudCover + '</h5>';
+        html += '<h5>Humidity: ' + posts.daily.data[i].humidity + '</h5>';
+        html += '<h5>Wind: ' + posts.daily.data[i].windGust + '</h5>';
+        html += '<h5>Pressure: ' + posts.daily.data[i].pressure + '</h5>';
+        html += '</div>';
+    }
+
+
+    return html;
+}
+
+$.get(corsUrl + darkSkyUrl + darkSkyAPIKey + "/" + lat + "," + lng + "/" + lang, {
+    daily: {
+        data: {
+            temperatureHigh: "",
+            temperatureLow: "",
+            cloudCover: "",
+            humidity: "",
+            windGust: "",
+            pressure: ""
+        }
+    }
+
+}).done(function(data) {
     console.log(data);
-
+    $('.currentSA').html(postsHtml(data));
 });
 
+
+
+
+
+mapboxgl.accessToken = mapboxKey;
+
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/dark-v10',
+    zoom: 10,
+    center: [-98.4916, 29.4252]
+});
